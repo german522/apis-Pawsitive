8b67b8b59fa63b41fd3b8342b015dbde17dcb54f
german522 <germancarroj@gmail.com>
2025-09-28 10:23:45 -0600

    Creación de un index para repositorios y concentración de los repositorios

---
+++ repositories/index.js
@@ -0,0 +1,15 @@
+const PersonaRepository = require('./PersonaRepository');
+const ClienteRepository = require('./ClienteRepository');
+const VeterinarioRepository = require('./VeterinarioRepository');
+const MascotaRepository = require('./MascotaRepository');
+const VacunaRepository = require('./VacunaRepository');
+const VacunaxMascotaRepository = require('./VacunaxMascotaRepository');
+
+module.exports = {
+    PersonaRepository,
+    ClienteRepository,
+    VeterinarioRepository,
+    MascotaRepository,
+    VacunaRepository,
+    VacunaxMascotaRepository
+};
