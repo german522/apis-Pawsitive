5207d6faed03b55925cb97ff22e8dc625d97d91e
german522 <germancarroj@gmail.com>
2025-10-06 22:25:07 -0600

    Se modificaron las migraciones para la creación de campos relacionados con la base de datos existente.

---
+++ migrations/20251007042350-add-verification-fields-to-personas.js
@@ -0,0 +1,27 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('personas', 'verificado', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    });
+
+    await queryInterface.addColumn('personas', 'codigo_verificacion', {
+      type: Sequelize.STRING(6),
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('personas', 'codigo_expiracion', {
+      type: Sequelize.DATE,
+      allowNull: true
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('personas', 'verificado');
+    await queryInterface.removeColumn('personas', 'codigo_verificacion');
+    await queryInterface.removeColumn('personas', 'codigo_expiracion');
+  }
+};
