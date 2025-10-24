476263a38b763473c866117494d8db9b568277e4
german522 <germancarroj@gmail.com>
2025-10-23 21:45:38 -0600

    Otro cambio en mascotas

--- migrations/20251022220000-create-cita.js
+++ migrations/20251022220000-create-cita.js
@@ -28,7 +28,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Mascota',
+          model: 'mascotas',
           key: 'id'
         },
         onDelete: 'CASCADE'
