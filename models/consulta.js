0511d066921065cb4bcb24ed4c2ecf7863d77912
german522 <germancarroj@gmail.com>
2025-11-04 20:28:21 -0600

    Se crearon los modelos y migraciones para Consultas. Se modificó el modelo de Citas para que pueda trabajarse con una relación 1 a 1 en Consultas

---
+++ models/consulta.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataTypes) => {
+  const Consulta = sequelize.define(
+    "Consulta",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      id_cita: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: {
+          model: 'citas',
+          key: 'id'
+        }
+      },
+      diagnostico: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      observaciones: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      tratamiento_sugerido: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    },
+    {
+      tableName: "consultas",
+      timestamps: true
+    }
+  );
+
+  Consulta.associate = (models) => {
+    Consulta.belongsTo(models.Cita, {
+      foreignKey: "id_cita",
+      as: "cita",
+      onDelete: "CASCADE"
+    });
+  };
+
+  return Consulta;
+};
