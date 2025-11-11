866b6470ec5941e3b1d61e23fe9f7ba7992f4c7e
german522 <germancarroj@gmail.com>
2025-11-11 14:55:31 -0600

    Existe servicios

--- migrations/20251030213713-create-servicios.js
+++ migrations/20251030213713-create-servicios.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Servicios", {
+    await queryInterface.createTable("servicios", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -63,6 +63,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Servicios");
+    await queryInterface.dropTable("servicios");
   }
 };
