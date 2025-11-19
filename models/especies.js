5196bb9eae0bfc62a35e6484b31da7443e437ec4
PamelaArgueta <argueta.martinez.22131@itsmante.edu.mx>
2025-11-18 20:26:56 -0600

    Se creo la migracion y el modelo de la tabla de especies

---
+++ models/especies.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Especie = sequelize.define(
+    "Especie",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nombre: {
+        type: DataTypes.ENUM(
+          "perro",
+          "gato",
+          "ave",
+          "reptil",
+          "roedor",
+          "otro"
+        ),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "especies",
+      timestamps: false,
+    }
+  );
+
+  Especie.associate = (models) => {
+    Especie.hasMany(models.Producto, {
+      foreignKey: "id_especie",
+      as: "productos",
+      onDelete: "SET NULL",
+    });
+  };
+
+  return Especie;
+};
