8553a158798f1dfa880b5e173f80f4a97a862ac9
PamelaArgueta <argueta.martinez.22131@itsmante.edu.mx>
2025-11-22 17:42:39 -0600

    Se modifico el modelo de personas para pdoer almacenar losdos nuevos campos que se añadieron a la tabla de personas para lo de recuperar contraseña.

--- models/persona.js
+++ models/persona.js
@@ -48,6 +48,14 @@ module.exports = (sequelize, DataTypes) => {
       codigo_expiracion: {
         type: DataTypes.DATE,
         allowNull: true
+      },
+      reset_token: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      reset_token_expires: {
+        type: DataTypes.DATE,
+        allowNull: true
       }
     },
     {
