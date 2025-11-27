080b2ec411ecd65eb9f275bec4ad3ec40f182b81
german522 <germancarroj@gmail.com>
2025-11-27 01:50:49 -0600

    Se implementa el middleware de receta para la creación y llamada de la misma en el controlador de Consulta

---
+++ utils/RecetaUtils.js
@@ -0,0 +1,20 @@
+const RecetaUtils = {
+
+    generateFolio: () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDay()).padStart(2, '0');
+        
+        const randomDigits = Math.floor(1000 + Math.random() * 9000);
+        return `RX-${year}${month}${day}-${randomDigits}`;
+    },
+
+    generateExpirationDate: (diasValidez = 30) => {
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + diasValidez);
+        return expirationDate;
+    }
+};
+
+module.exports = RecetaUtils;
